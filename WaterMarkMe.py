ddb73ecd37147c6877c8aa63e17fc4d229a09ebb
Mike <taurenmike@gmail.com>
2018-03-09 18:20:56 -0600

    Added threading for images. Also some documentation

--- WaterMarkMe.py
+++ WaterMarkMe.py
@@ -1,9 +1,20 @@
 import sys
 import os
+from threading import Thread
 from PIL import Image, ImageDraw, ImageFont
+
+# TODO: Create a progress bar.
 
 
 def watermark(path, filename, save_path, text='Camis Photography'):
+    """
+    This method actually adds the watermark text to the image.
+    :param path: Path to the image
+    :param filename: image name
+    :param save_path: The path to the destination folder for the new file
+    :param text: The text to add. Default is Camis Photography
+    :return:
+    """
     image = Image.open(os.path.join(path, filename)).convert('RGBA')
     image_watermark = Image.new('RGBA', image.size, (255, 255, 255, 0))
 
@@ -24,6 +35,11 @@ def watermark(path, filename, save_path, text='Camis Photography'):
 
 
 def make_marked_folder(folder_path):
+    """
+    This creates a folder for the new watermarked images
+    :param folder_path: The path to the current folder
+    :return: the new folder
+    """
     new_folder = folder_path + 'WaterMarked'
     os.makedirs(new_folder)
     print("Folder Created: " + new_folder)
@@ -31,9 +47,18 @@ def make_marked_folder(folder_path):
 
 
 def mark_images(target_folder, save_folder):
+    """
+    TODO: Recycle a pool of threads instead of creating one for each image.
+    This method cycles through the images and starts a thread for each image.
+    :param target_folder: The folder containing the images
+    :param save_folder: the folder that all teh images should be saved in
+    :return:
+    """
     for filename in os.listdir(target_folder):
         if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
-            watermark(target_folder, filename, save_folder)
+            image = Thread(target=watermark, args=(target_folder, filename, save_folder,))
+            image.start()
+            image.join()
 
 
 if __name__ == "__main__":
